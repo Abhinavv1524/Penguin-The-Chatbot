a157d080d09f23e79d86437f25e2c1face32f92b
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-10-03 12:29:01 +0530

    Remvoing the API key

--- app/src/main/java/com/example/penguin/API.java
+++ app/src/main/java/com/example/penguin/API.java
@@ -2,5 +2,5 @@ package com.example.penguin;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/chat/completions";
-    public static String API = "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";
+    public static String API = "";
 }
